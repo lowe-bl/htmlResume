0ff09d9a2cf52e7aa9fa31ae44d03a93163aa117
Bonnie Lowe <bonnie.lowe@accenture.com>
2022-09-23 19:08:11 -0500

    more js changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Contact from "./Contact.js";
+
 const formInfo = document.getElementById("formInfo");
 
 let hasJob = false;
@@ -50,30 +52,4 @@ for (let x = 0; x < experience.length; x++) {
 
 }
 
-class Contact  {
 
-    constructor(form) {
-        this.fullName = form.elements["fullName"].value;
-        this.email = form.elements["email"].value;
-        this.subject = form.elements["subject"].value;
-        this.body = form.elements["msg"].value;
-
-    }
-
-    fullName = "";
-    email = "";
-    subject = "";
-    body = "";
-
-    send() {
-        console.info(this.formatMessage())
-        showMessage("We're not sending emails yet...feature for version 2.");
-    }
-
-    formatMessage() {
-        return `To: ${this.fullName}
-                Email: ${this.email}
-                Subject: ${this.subject}
-                Body: ${this.body} `;
-    }
-};
